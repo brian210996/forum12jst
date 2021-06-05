eaf627ca979da5c3500fbc283cce8bf711ede01f
brian210996 <84860438+brian210996@users.noreply.github.com>
2021-06-05 23:28:55 +0700

    Update index.js

--- api/index.js
+++ api/index.js
@@ -7,7 +7,7 @@ const cls_model = require('./sdk/cls_model.js'); //cls
 
 // Bot Setting
 const TelegramBot = require('node-telegram-bot-api');
-const token = '1827194583:AAHuIsCFYz9vk_Bmpacq2Qk7_h9rJke0t8M'
+const token = '1827194583:AAHuIsCFYz9vk_Bmpacq2Qk7_h9rJke0t8M';
 const bot = new TelegramBot(token, {polling: true});
 
 state = 0;
