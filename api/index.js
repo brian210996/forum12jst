00e1de83b88119b7608d030dc2751b0798649d61
brian210996 <84860438+brian210996@users.noreply.github.com>
2021-06-05 23:20:52 +0700

    Update index.js

--- api/index.js
+++ api/index.js
@@ -2,8 +2,8 @@ var express = require('express');
 var r = express.Router();
 
 // load pre-trained model
-const model = require('./sdk/model.js');
-const cls_model = require('./sdk/cls_model.js');
+const model = require('./sdk/model.js'); //predict
+const cls_model = require('./sdk/cls_model.js'); //cls
 
 // Bot Setting
 const TelegramBot = require('node-telegram-bot-api');
@@ -11,22 +11,23 @@ const token = '1827194583:AAHuIsCFYz9vk_Bmpacq2Qk7_h9rJke0t8M'
 const bot = new TelegramBot(token, {polling: true});
 
 state = 0;
-// Main menu bot
+// Main Menu Bot
 bot.onText(/\/start/, (msg) => { 
     bot.sendMessage(
         msg.chat.id,
         `hello ${msg.chat.first_name}, welcome...\n
-        click predict`
-    );   
-    state = 0;
+        click  /predict`
+    );
+    state = 0;  
 });
 
-// Input Requieres i and r
+
+// input requires i and r
 bot.onText(/\/predict/, (msg) => { 
     bot.sendMessage(
         msg.chat.id,
-        `masukan nila i|v contohnya 9|9`
-    );  
+        `input nilai i|v example 7|5`
+    );   
     state = 1;
 });
 
@@ -35,50 +36,51 @@ bot.on('message', (msg) => {
         s = msg.text.split("|");
         model.predict(
             [
-                parseFloat(s[0]), //string to float
+                parseFloat(s[0]), // string to float
                 parseFloat(s[1])
             ]
         ).then((jres1)=>{
             console.log(jres1);
-            
-            cls_model.classify([parseFloat(s[0]), parseFloat(s[1]), parseFloat(jres1[0]), parseFloat(jres1[1])]).then((jres2)=>{
+
+	    cls_model.classify([parseFloat(s[0]), parseFloat(s[1]), parseFloat(jres1[0]), parseFloat(jres1[1])]).then((jres2)=>{
                 bot.sendMessage(
-                    msg.chat.id,
-                    'nilai v yang diprediksi adalah ${jres1[0]} volt'
-                    );
+                        msg.chat.id,
+                        `nilai v yang diprediksi adalah ${jres1[0]} volt`
+                );   
                 bot.sendMessage(
-                    msg.chat.id,
-                    'nilai p yang diprediksi adalah ${jres1[1]} watt'
-                    );
-                bot.sendMessage(
-                    msg.chat.id,
-                    'klasifikasi tegangan ${jres2}'
-                    );
-                state = 0;
-            })
-        })
+                        msg.chat.id,
+                        `nilai p yang diprediksi adalah ${jres1[1]} watt`
+    		);
+		bot.sendMessage(
+                        msg.chat.id,
+                        `Klasifikasi Tegangan ${jres2}`
+		);
+		state = 0;
+	   })
+	})
     }else{
-        bot.sendMessage(
-        msg.chat.id,
-            'Please Click /start'
-            );
+	bot.sendMessage(
+	msg.chat.id,
+	    `Please Click /start`
+	);
         state = 0;
     }
 })
 
+
 // routers
 r.get('/predict/:i/:r', function(req, res, next) {    
     model.predict(
-        [
+ 	[
             parseFloat(req.params.i), // string to float
             parseFloat(req.params.r)
         ]
     ).then((jres)=>{
-        res.json(jres);
+        res.json.(jres);
     })
 });
 
-//routers
+// routers
 r.get('/classify/:i/:r', function(req, res, next) {    
     model.predict(
         [
@@ -87,16 +89,17 @@ r.get('/classify/:i/:r', function(req, res, next) {
         ]
     ).then((jres)=>{
         cls_model.classify(
-            [
-                parseFloat(req.params.i), // string to float
-                parseFloat(req.params.r),
-                parseFloat(jres[0]), 
-                parseFloat(jres[1])
-            ]
-         ).then((jres)=>{
-             res.json(jres, jres_})
-        })
+	    [
+		parseFloat(req.params.i), // string to float
+		parseFloat(req.params.r),
+		parseFloat(jres[0]),
+		parseFloat(jres[1])
+	    ]
+	).then((jres_)=>{
+	    res.json({jres, jres_})
+	})
     })
-});
+ });
 
 module.exports = r;
+© 2021 GitHub, Inc.
